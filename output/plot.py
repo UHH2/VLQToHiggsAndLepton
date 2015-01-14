aca8c194b9eb7e5cf0a05642aeab0b1a05faf76e
Heiner Tholen <heiner.tholen@cern.ch>
2015-01-14 11:41:46 +0100

    normalized plots

--- output/plot.py
+++ output/plot.py
@@ -5,13 +5,22 @@ ROOT.gROOT.SetBatch()
 
 import os
 import time
-import varial.tools as t
+import varial.tools
 
 dirname = 'FirstPlots_VLQToHiggsAndLepton'
 
-p = t.mk_rootfile_plotter(name=dirname)
+
+def plotter_factory(**kws):
+    kws['hook_loaded_histos'] = varial.generators.gen_norm_to_integral
+    return varial.tools.Plotter(**kws)
+
+p = varial.tools.mk_rootfile_plotter(
+    name=dirname,
+    plotter_factory=plotter_factory,
+    combine_files=True
+)
 time.sleep(1)
 p.run()
-t.WebCreator().run()
+varial.tools.WebCreator().run()
 os.system('rm -r ~/www/%s' % dirname)
 os.system('mv -f %s ~/www' % dirname)

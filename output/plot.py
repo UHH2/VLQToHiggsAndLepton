18af56f913b21ef7b665c16c83642d81383ad6cc
Heiner Tholen <heiner.tholen@cern.ch>
2015-01-19 20:00:43 +0100

    automatic efficiency plots by looking for _sub and _tot endings in histogram names

--- output/plot.py
+++ output/plot.py
@@ -11,8 +11,38 @@ import varial.tools
 dirname = 'FirstPlots_VLQToHiggsAndLepton'
 
 
+def make_eff_graphs(wrps):
+    token = lambda w: w.legend + ":" + "/".join(w.in_file_path)[:-4]
+    subs, tots = {}, {}
+    for wrp in wrps:
+        yield wrp
+        if wrp.name.endswith('_sub'):
+            t = token(wrp)
+            if t in tots:
+                yield varial.operations.eff((wrp, tots.pop(t)))
+            else:
+                subs[t] = wrp
+        elif wrp.name.endswith('_tot'):
+            t = token(wrp)
+            print t
+            if t in subs:
+                yield varial.operations.eff((subs.pop(t), wrp))
+            else:
+                tots[t] = wrp
+
+
+def norm_histos_to_integral(wrps):
+    for wrp in wrps:
+        if isinstance(wrp, varial.wrappers.HistoWrapper):
+            yield varial.operations.norm_to_integral(wrp)
+        else:
+            yield wrp
+
+
 def loader_hook(wrps):
-    wrps = varial.generators.gen_norm_to_integral(wrps)
+    wrps = make_eff_graphs(wrps)
+    wrps = sorted(wrps, key=lambda w: w.type + ':' + w.name)
+    wrps = norm_histos_to_integral(wrps)
     for w in wrps:
         if 'TH1' in w.type and w.histo.GetXaxis().GetTitle() == '':
             w.histo.GetXaxis().SetTitle(w.histo.GetTitle())

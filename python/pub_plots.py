327601478ee66ab49094f9a6c4c3449de726d719
Heiner Tholen <heiner.tholen@cern.ch>
2016-07-25 13:25:45 +0200

    pretty plots / LE round 2

--- python/pub_plots.py
+++ python/pub_plots.py
@@ -46,16 +46,16 @@ plot_config = {  #                   lumi legend x1 x2 y1 y2    CMS pos   chan p
 'sel_eff_el.pdf':                   (0,   (.70, .90, .65, .85), (.2, .8), (.2, .6, 1),   1.9),
 'sel_eff_mu.pdf':                   (0,   (.70, .90, .20, .40), (.2, .8), (.65,.255,2),  1.9),
 
-'selblock_h_mass_lin.pdf':          (2.3, (.65, .85, .16, .65), (.9, .8), (.65,.8, 2),  5500),
-'selblock_primary_el_pt_lin.pdf':   (2.2, (.65, .85, .16, .65), (.9, .8), (.65,.8, 1),  1100),
-'selblock_primary_mu_pt_lin.pdf':   (2.3, (.65, .85, .16, .65), (.9, .8), (.65,.8, 2),  6000),
-'selblock_ST_lin.pdf':              (2.2, (.65, .85, .36, .85), (.2, .8), (.2, .7, 1),  1050),
+'selblock_h_mass_lin.pdf':          (2.3, (.62, .82, .24, .73), (.9, .8), (.61,.8, 2),  5500),
+'selblock_primary_el_pt_lin.pdf':   (2.2, (.62, .82, .24, .73), (.9, .8), (.61,.8, 1),  1100),
+'selblock_primary_mu_pt_lin.pdf':   (2.3, (.62, .82, .24, .73), (.9, .8), (.61,.8, 2),  6000),
+'selblock_ST_lin.pdf':              (2.2, (.62, .82, .36, .85), (.2, .8), (.2, .7, 1),  1050),
 
-'Sideband__el_lin.pdf':             (2.2, (.67, .87, .36, .85), (.2, .8), (.2, .7, 1),   150),
-'Sideband__mu_lin.pdf':             (2.3, (.67, .87, .36, .85), (.2, .8), (.2, .7, 2),   800),
+'Sideband__el_lin.pdf':             (2.2, (.18, .37, .36, .85), (.9, .8), (.9, .7, 1),   150),
+'Sideband__mu_lin.pdf':             (2.3, (.62, .82, .36, .85), (.2, .8), (.2, .7, 2),   800),
 
-'Sideband_vs_SignalRegion__el.pdf': (0,   (.65, .90, .62, .85), (.2, .8), (.2,.62, 1),  0.19),
-'Sideband_vs_SignalRegion__mu.pdf': (0,   (.65, .90, .62, .85), (.2, .8), (.2,.62, 2),   0.0),
+'Sideband_vs_SignalRegion__el.pdf': (0,   (.62, .82, .67, .85), (.2, .8), (.2,.62, 1),  0.19),
+'Sideband_vs_SignalRegion__mu.pdf': (0,   (.62, .82, .67, .85), (.2, .8), (.2,.62, 2),   0.0),
 
 'SignalRegion__el_lin.pdf':         (0,   (.20, .40, .46, .85), (.9, .8), (.2,.38, 1),  13.5),
 'SignalRegion__mu_lin.pdf':         (0,   (.60, .80, .46, .85), (.2, .8), (.2,.62, 2),  50.0),
@@ -64,18 +64,18 @@ plot_config = {  #                   lumi legend x1 x2 y1 y2    CMS pos   chan p
 'SignalRegion_bkg__mu_lin.pdf':     (2.3, (.65, .85, .55, .85), (.2, .8), (.2, .7, 2),  35.0),
 'SignalRegion_bkg__comb_lin.pdf':   (2.3, (.65, .85, .55, .85), (.2, .8), (.2, .7, 3),  45.0),
 
-'tlep_mass_lin.pdf':                (2.3, (.65, .85, .16, .65), (.9, .8), (.65,.8, 2),  2000),
-'tlep_pt_lin.pdf':                  (2.3, (.65, .85, .16, .65), (.9, .8), (.65,.8, 2),  2700),
+'tlep_mass_lin.pdf':                (2.3, (.62, .82, .24, .73), (.9, .8), (.61,.8, 2),  2000),
+'tlep_pt_lin.pdf':                  (2.3, (.62, .82, .24, .73), (.9, .8), (.61,.8, 2),  2700),
 
-'TpBLH_limits.pdf':                 (2.3, (.30, .50, .63, .85), (.9, .8), (.9, .6, 0),   99.),
-'TpBRH_limits.pdf':                 (2.3, (.30, .50, .63, .85), (.9, .8), (.9, .6, 0),   99.),
-'TpTLH_limits.pdf':                 (2.3, (.30, .50, .63, .85), (.9, .8), (.9, .6, 0),   99.),
-'TpTRH_limits.pdf':                 (2.3, (.30, .50, .63, .85), (.9, .8), (.9, .6, 0),   99.),
+'TpBLH_limits.pdf':                 (2.3, (.38, .58, .58, .85), (.2, .8), (.9, .6, 0),   99.),
+'TpBRH_limits.pdf':                 (2.3, (.38, .58, .58, .85), (.2, .8), (.9, .6, 0),   99.),
+'TpTLH_limits.pdf':                 (2.3, (.38, .58, .58, .85), (.2, .8), (.9, .6, 0),   99.),
+'TpTRH_limits.pdf':                 (2.3, (.38, .58, .58, .85), (.2, .8), (.9, .6, 0),   99.),
 
-'TpBLH_coupling_limits.pdf':        (2.3, (.50, .79, .68, .85), (.2, .8), (.2, .6, 0),   0.0),
-'TpBRH_coupling_limits.pdf':        (2.3, (.50, .79, .68, .85), (.2, .8), (.2, .6, 0),   0.0),
-'TpTLH_coupling_limits.pdf':        (2.3, (.50, .79, .68, .85), (.2, .8), (.2, .6, 0),   9.9),
-'TpTRH_coupling_limits.pdf':        (2.3, (.50, .79, .68, .85), (.2, .8), (.2, .6, 0),   9.9),
+'TpBLH_coupling_limits.pdf':        (2.3, (.38, .58, .63, .85), (.2, .8), (.2, .6, 0),   0.0),
+'TpBRH_coupling_limits.pdf':        (2.3, (.38, .58, .63, .85), (.2, .8), (.2, .6, 0),   0.0),
+'TpTLH_coupling_limits.pdf':        (2.3, (.38, .58, .63, .85), (.2, .8), (.2, .6, 0),   9.9),
+'TpTRH_coupling_limits.pdf':        (2.3, (.38, .58, .63, .85), (.2, .8), (.2, .6, 0),   9.9),
 }
 
 
@@ -96,9 +96,12 @@ def handle_plot(name):
     canv_prims = list(c.GetListOfPrimitives())
     legend = next(p for p in canv_prims if isinstance(p, ROOT.TLegend))
     if isinstance(canv_prims[0], ROOT.TPad) and isinstance(canv_prims[1], ROOT.TPad):
-        main_pad, second_pad = canv_prims[0], canv_prims[1]
+        main_pad, second_pad, size_factor = canv_prims[0], canv_prims[1], 0.8
     else:
-        main_pad, second_pad = c, c
+        main_pad, second_pad, size_factor = c, c, 1
+
+    def get_pos(old_pos):
+        return old_pos*size_factor + (1 - size_factor)
 
     main_hists = list(
         p
@@ -111,31 +114,29 @@ def handle_plot(name):
     main_pad.SetRightMargin(0.05)
     second_pad.SetRightMargin(0.05)
 
-    # area in coupling plots
-    if '_coupling_' in save_name:
-        obs_lim = main_hists[-1]
-        n = obs_lim.GetN()
-        obs_lim.SetPoint(n, 1800, 40)
-        obs_lim.SetPoint(n+1, 700, 40)
-        obs_lim.SetFillStyle(3344)
-        obs_lim.SetFillColor(ROOT.kGray + 1)
-        obs_lim.SetLineColor(ROOT.kGray + 1)
-        obs_lim.SetLineWidth(2)
-        obs_lim.Draw('F')
-        legend_entry = legend.GetListOfPrimitives()[0]
-        legend_entry.SetObject(obs_lim)
-        legend_entry.SetOption('F')
-        # legend_entry.SetFillColor(ROOT.kGray + 1)
-        # legend_entry.SetFillStyle(3244)
+#    # area in coupling plots
+#    if '_coupling_' in save_name:
+#        obs_lim = main_hists[-1]
+#        n = obs_lim.GetN()
+#        obs_lim.SetPoint(n, 1800, 40)
+#        obs_lim.SetPoint(n+1, 700, 40)
+#        obs_lim.SetFillStyle(3344)
+#        obs_lim.SetFillColor(ROOT.kGray + 1)
+#        obs_lim.SetLineColor(ROOT.kGray + 1)
+#        obs_lim.SetLineWidth(2)
+#        obs_lim.Draw('F')
+#        legend_entry = legend.GetListOfPrimitives()[0]
+#        legend_entry.SetObject(obs_lim)
+#        legend_entry.SetOption('F')
+#        # legend_entry.SetFillColor(ROOT.kGray + 1)
+#        # legend_entry.SetFillStyle(3244)
 
     # move legend
+    # legend.SetTextSize(1.1 * legend.GetTextSize())
     legend.SetX1NDC(x1)
     legend.SetX2NDC(x2)
-    legend.SetY1NDC(y1)
-    legend.SetY2NDC(y2)
-    #  legend.Draw()
-
-    c.cd()
+    legend.SetY1NDC(get_pos(y1))
+    legend.SetY2NDC(get_pos(y2))
 
     # lumi / sqrt s text
     lumi_line = ('%.1f fb^{-1} (13 TeV)' % lumi) if lumi else '(13 TeV)'
@@ -144,29 +145,26 @@ def handle_plot(name):
     lumi_txt.SetTextColor(ROOT.kBlack)
     lumi_txt.SetTextFont(42)
     lumi_txt.SetTextAlign(31)
-    lumi_txt.SetTextSize(0.06)
+    lumi_txt.SetTextSize(0.06 * size_factor)
     lumi_txt.SetFillStyle(0)
     lumi_txt.SetBorderSize(0)
     lumi_txt.Draw('same')
 
     # CMS preliminary text
-    cmsTextSize = 0.075
+    cmsTextSize = 0.075 * size_factor
     latex = ROOT.TLatex()
     latex.SetNDC()
 
     latex.SetTextFont(61)
     latex.SetTextSize(cmsTextSize)
     latex.SetTextAlign(31 if cms_x > 0.5 else 11)
-    latex.DrawLatex(cms_x, cms_y, 'CMS')
+    latex.DrawLatex(cms_x, get_pos(cms_y), 'CMS')
 
     latex.SetTextFont(52)
     latex.SetTextAlign(31 if cms_x > 0.5 else 11)
     latex.SetTextSize(0.76 * cmsTextSize)
-    if lumi:
-        pass  # latex.DrawLatex(cms_x, cms_y - 1.0*cmsTextSize, 'Preliminary')
-    else:
-        latex.DrawLatex(cms_x, cms_y - 1.0*cmsTextSize, 'Simulation')
-        # latex.DrawLatex(cms_x, cms_y - 1.76*cmsTextSize, 'Preliminary')
+    if not lumi:
+        latex.DrawLatex(cms_x, get_pos(cms_y - 1.0*cmsTextSize), 'Simulation')
 
     # electron / muon channel
     if chan:
@@ -175,26 +173,30 @@ def handle_plot(name):
         latex.SetTextFont(42)
         latex.SetTextAlign(31 if chan_x > 0.5 else 11)
         latex.SetTextSize(0.65 * cmsTextSize)
-        latex.DrawLatex(chan_x, chan_y, chan_txts[chan])
-        latex.DrawLatex(chan_x, chan_y - 0.6*cmsTextSize, chan_txt2)
+        latex.DrawLatex(chan_x, get_pos(chan_y), chan_txts[chan])
+        latex.DrawLatex(chan_x, get_pos(chan_y - 0.7*cmsTextSize), chan_txt2)
 
     # darker shade on uncertainties
     for obj in main_hists:
         if 'uncert.' in obj.GetTitle():
             obj.SetFillColor(ROOT.kBlack)
 
-    if main_pad != second_pad:
-        second_pad.GetListOfPrimitives()[-2].SetFillColor(ROOT.kBlack)
-
     # x and y axes
     first_obj = main_hists[0]
     x_axis = second_pad.GetListOfPrimitives()[1].GetXaxis()
     y_axis = first_obj.GetYaxis()
-    y_axis.SetTitleOffset(1.25)
+    y_axis.SetTitleOffset(1.4)
     y_axis.CenterTitle(0)
     y_axis.SetNoExponent()
-
     x_axis.SetTitle(x_axis.GetTitle().replace('/ GeV', '(GeV)'))
+
+    # fix signal line width
+    if any(isinstance(h, ROOT.THStack) for h in main_hists) and 4 < len(main_hists):
+        offset = 1 if len(main_hists) == 5 else 2
+        for i in xrange(3):
+            main_hists[-offset-i].SetLineWidth(2)
+            main_hists[-offset-i].SetLineColor(602)
+            main_hists[-offset-i].SetLineStyle(i+1)
 
     if y_scale_max:
         first_obj.SetMaximum(y_scale_max)
@@ -203,14 +205,13 @@ def handle_plot(name):
         main_pad.SetLogy()
 
     if save_name.endswith('_limits'):
+        legend.SetTextSize(1.2 * legend.GetTextSize())
         x_axis = first_obj.GetXaxis()
         x_axis.SetRangeUser(700, 1700)
 
     if name.startswith('sel_eff_'):
-        legend.SetTextSize(1.4 * legend.GetTextSize())
+        legend.SetTextSize(1.3 * legend.GetTextSize())
         y_axis.SetTitleSize(1.2 * y_axis.GetTitleSize())
-    else:
-        legend.SetTextSize(1.1 * legend.GetTextSize())
 
     # more detail fixings...
     if save_name.endswith('H_limits'):
@@ -226,7 +227,7 @@ def handle_plot(name):
         entries[-1].GetObject().SetLineColor(1)
 
     if name.startswith('Sideband_vs_SignalRegion__'):
-        legend.SetTextSize(1.3 * legend.GetTextSize())
+        # legend.SetTextSize(1.3 * legend.GetTextSize())
         entries_tlist = legend.GetListOfPrimitives()
         entries = list(entries_tlist)
         entries[0].SetLabel('signal region')
@@ -239,11 +240,8 @@ def handle_plot(name):
         entries_tlist.Add(entries[2])
         entries_tlist.Add(entries[1])
 
-
     c.Modified()
     c.Update()
-    main_pad.Modified()
-    main_pad.Update()
     c.SaveAs('PlotBeautifier/'+name)
 
 
